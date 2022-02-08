4b6fad291f77af02f46b3a8d9eebe4a2ec5aad98
Verisimilitude <acharyapiyush1@outlook.com>
2022-02-07 17:47:29 -0800

    Update Fortune_Teller.py

--- Fortune_Teller.py
+++ Fortune_Teller.py
@@ -1,14 +1,7 @@
-"""
-LESSON: 2.3 - Booleans
-EXERCISE: Fortune Teller
-"""
-
 #### ---- OUTER INPUT ---- ####
 
 # Ask the user for a number 1 - 4. Assign result to the
 # variable outer_choice
-# ---> TEST AFTER THIS LINE <--- #
-
 outer_choice = int(input("Please choose 1, 2, 3, 4"))
 
 #### ---- ODD OUTER CHOICES ---- ####
@@ -19,37 +12,31 @@ if outer_choice == 1 or outer_choice == 3:
 
     # Have the user choose "red", "blue", "green" or
     # "yellow". Assign the result to inner_choice
-    # ---> TEST AFTER THIS LINE <--- #
     inner_choice = input("Please choose a color: red, blue, green")
 
     # IF inner_choice is EQUAL TO "red"
     if inner_choice == "red":
-        print("You'll win a million dollars!")
-
+        
         # Print your first fortune
-        # ---> TEST AFTER THIS LINE <--- #
+        print("You'll win a million dollars!")
 
 
     # ELIF inner_choice is EQUAL TO "blue"
     elif inner_choice == "blue":
 
         # Print your second fortune
-        # ---> TEST AFTER THIS LINE <--- #
-        print("You will travel to brazil")
+        print("You will travel to Brazil")
 
     # ELIF inner_choice is EQUAL TO "green"
     elif inner_choice == "green":
-        print("You will get a frog soon")
-
+        
         # PRINT your third fortune
-        # ---> TEST AFTER THIS LINE <--- #
-
+        print("You will get a frog soon")
 
     # ELIF inner_choice is EQUAL TO "yellow"
     elif inner_choice == "yellow":
 
         # Print your fourth fortune
-        # ---> TEST AFTER THIS LINE <--- #
         print("You will lose something valuable")
 
 
@@ -61,42 +48,30 @@ elif outer_choice == 2 or outer_choice == 4:
 
     # Have the user choose "black", "purple", "pink",
     # or "white". Assign the result to inner_choice
-    # ---> TEST AFTER THIS LINE <--- #
     inner_choice = input("Please choose a color: black, purple, pink or white")
 
     # IF inner_choice is EQUAL TO "black"
     if inner_choice== "black":
-        print("You will win a lifetime supply of candy")
-
+        
         # Print your fifth fortune
-        # ---> TEST AFTER THIS LINE <--- #
-
+        print("You will win a lifetime supply of candy")
 
     # ELIF inner_choice is EQUAL TO "purple"
     elif inner_choice == "purple":
-        print("You will win a disney cruise")
-
+        
         # Print your sixth fortune
-        # ---> TEST AFTER THIS LINE <--- #
-
+        print("You will win a disney cruise")
 
     # ELIF inner_choice is EQUAL TO "pink"
     elif inner_choice == "pink":
-        print("Someone has a crunch on you!")
-
+        
         # Print your seventh fortune
-        # ---> TEST AFTER THIS LINE <--- #
-
+        print("Someone has a crunch on you!")
 
     # ELIF inner_choice is EQUAL TO "white"
     elif inner_choice == "white":
         print("You will be haunted")
 
         # Print your eighth fortune
-        # ---> TEST AFTER THIS LINE <--- #
     else:
         print("You will have to eat spinach for breakfast everyday.")
-
-
-# Turn in your Coding Exercise.
-
